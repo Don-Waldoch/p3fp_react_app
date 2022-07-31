8fdfe37dcb60513d3e096108b852bb877548d942
unknown <waldoch.don@gmail.com>
2022-07-31 17:28:47 -0500

    Styled & Labeled Videos

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -18,15 +18,23 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
           gap: { lg: '110px', xs: '0' }
         }}
       >
-        {exerciseVideos?.slice(0, 3).map((item, index) => (
+        {exerciseVideos?.slice(0, 6).map((item, index) => (
           <a
             key={index}
-            className="exercises-video"
+            className="exercise-video"
             href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
             target="_blank"
             rel="noreferrer"
           >
             <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <Box>
+              <Typography variant="h6" color="#000000">
+                {item.video.title}
+              </Typography>
+              <Typography variant="h6" color="#000000">
+                {item.video.channelName}
+              </Typography>
+            </Box>
           </a>
         ))}
       </Stack>
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_EXERCISEDB_HOST
   }
 }
 
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_YOUTUBE_HOST
   }
 }
 
