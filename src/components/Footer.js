53d0a8636907662fade0d898b1dc97b657527203
unknown <waldoch.don@gmail.com>
2022-08-01 10:20:10 -0500

    Added my logos to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
-import Logo from '../assets/images/Logo-1.png';
+import Logo from '../assets/images/Dwalz.png';
 
 const Footer = () => {
   return (
-    <Box mt="80px" bgcolor="#F8F8F8">
+    <Box mt="80px" bgcolor="#F4F4F4">
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
-        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <img src={Logo} alt="logo" width="200px" height="32px" />
       </Stack>
       <Typography variant="h5" sx={{
           fontSize: { lg: '28px', xs: '20px' }
         }} mt="20px" textAlign="center" pb="40px">
-        Made with ❤️ by JavaScript Mastery
+        Made with 💜 by Don Waldoch
       </Typography>
     </Box>
   )
