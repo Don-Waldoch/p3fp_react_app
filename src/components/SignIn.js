7283268cf20fc9c882342c6c6e1e7e9de6c42197
unknown <waldoch.don@gmail.com>
2022-08-01 17:55:40 -0500

    SHA-3 hashing, AES encryption

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -14,17 +14,43 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import Copyright from './Copyright';
-
+const CryptoJS = require('crypto-js');
 const theme = createTheme();
 
 export default function SignIn() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+
+    let ClearText = data.get('password');
+    let CodedText = encryptWithAES(ClearText);
+    let PlainText = decryptWithAES(CodedText);
+    // console.log(ClearText);
+    // console.log(CodedText);
+    // console.log(PlainText);
+
+    var hash = CryptoJS.SHA3(ClearText, { outputLength: 256 }).toString();
+    console.log(hash);
+    let CodedHash = encryptWithAES(hash);
+    console.log(CodedHash);
+
+    // console.log({
+    //   email: data.get('email'),
+    //   password: data.get('password'),
+    // });
+  };
+
+  const encryptWithAES = (ClearText) => {
+    const passphrase = 'hWmZq3t6w9z$C&F)J@NcRfUjXn2r5u7x';
+    const CodedText = CryptoJS.AES.encrypt(ClearText, passphrase).toString();
+    return CodedText;
+  };
+
+  const decryptWithAES = (CodedText) => {
+    const passphrase = 'hWmZq3t6w9z$C&F)J@NcRfUjXn2r5u7x';
+    const bytes = CryptoJS.AES.decrypt(CodedText, passphrase);
+    const ClearText = bytes.toString(CryptoJS.enc.Utf8);
+    return ClearText;
   };
 
   return (
