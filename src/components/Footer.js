6289c850076a370c0ea92c2fe804223dffbae4ad
unknown <waldoch.don@gmail.com>
2022-08-01 09:36:49 -0500

    Same Equipment & Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor="#F8F8F8">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+      </Stack>
+      <Typography variant="h5" sx={{
+          fontSize: { lg: '28px', xs: '20px' }
+        }} mt="20px" textAlign="center" pb="40px">
+        Made with ❤️ by JavaScript Mastery
+      </Typography>
+    </Box>
   )
 }
 
--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -7,13 +7,22 @@ import Loader from './Loader';
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
     <Box sx={{  mt: { lg: '100px', xs: '0' }}} p="20px">
-      <Typography variant="h3" mb="5">
+      <Typography variant="h3" mb="33px">
         Similar <span style={{ color: '#9C27B0', }}>Target Muscle</span> exercises
       </Typography>
       <Stack direction="row" sx={{ p: '2', position: 'relative' }}>
         {
           targetMuscleExercises.length !== 0 ?
           <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />
+        }
+      </Stack>
+      <Typography variant="h3" mb="33px">
+        Same <span style={{ color: '#9C27B0', }}>Equipment</span> exercises
+      </Typography>
+      <Stack direction="row" sx={{ p: '2', position: 'relative' }}>
+        {
+          equipmentExercises.length !== 0 ?
+          <HorizontalScrollbar data={equipmentExercises} /> : <Loader />
         }
       </Stack>
     </Box>
