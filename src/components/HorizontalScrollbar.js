762abc6f53101e746508da8be63761a80b0b44ec
unknown <waldoch.don@gmail.com>
2022-08-02 09:25:29 -0500

    Added Sign Up to Navbar

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -12,8 +12,8 @@ const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="right-arrow" />
+    <Typography onClick={() => scrollPrev()} className="left-arrow">
+      <img src={LeftArrowIcon} alt="left-arrow" />
     </Typography>
   );
 };
@@ -22,7 +22,7 @@ const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollNext()} className="left-arrow">
+    <Typography onClick={() => scrollNext()} className="right-arrow">
       <img src={RightArrowIcon} alt="right-arrow" />
     </Typography>
   );
